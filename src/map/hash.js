6f06dd6bb711984f7520103981a9c6368dc18dcf
Lukas Wagner <lukas.wagner@student.hpi.de>
2021-02-17 22:17:42 +0100

    implement collision-free hashing

--- src/map/hash.js
+++ src/map/hash.js
@@ -3,10 +3,12 @@
 const crypto = require('crypto');
 
 function hash(input, salt, algorithm, range) {
+    const maxLength = range.toString(16).length;
     const hashString =
         crypto.createHmac(algorithm, salt)
             .update(input)
-            .digest('hex');
+            .digest('hex')
+            .substring(0, maxLength);
     const hashNum = parseInt(hashString, 16);
     return hashNum % range;
 }
--- src/map/map.js
+++ src/map/map.js
@@ -16,25 +16,47 @@ function map({
 }) {
     const students = readStudents(studentsFile, format);
 
-    console.log('Searching for distinct mapping...');
-    const maxSecret = 10000;
-    const logStep = 100;
-    let salt = 0;
+    if (students.length > numHashes) {
+        console.error('More students than possible hashes! Aborting!');
+        return;
+    }
+
+    if (students.length > numHashes * 0.8) {
+        console.warn(
+            'Hash range heavily utilized',
+            `(mapping ${students.length} to ${numHashes} hashes).`,
+            'This may take a moment.');
+    };
+
+    const candidates = new Set(students);
+    const used = new Set();
     let hashes = [];
 
-    do {
-        salt++;
-        if (salt % logStep === 0) console.log('Iteration', salt);
-        const secretString = salt.toString();
-        hashes = students.map((s) => hash(s, secretString, 'md5', numHashes));
-    } while (collision(hashes) && salt < maxSecret)
+    const maxSalt = 10000;
+    let salt = 0;
 
-    if (salt === maxSecret) {
+    do {
+        const saltString = salt.toString();
+        candidates.forEach((c) => {
+            const h = hash(c, saltString, 'md5', numHashes);
+            if (used.has(h)) return;
+            used.add(h);
+            hashes[students.findIndex((s) => s === c)] = h;
+            candidates.delete(c);
+        })
+        salt++;
+    } while (candidates.size > 0 && salt < maxSalt)
+
+    if (salt === maxSalt) {
         console.error('Could not find a distinct mapping!');
         return;
     }
 
-    console.log(`Found distinct mapping with salt`, salt);
+    // sanity check. should be ensured by using sets, but just to make sure...
+    if (hashes.some((h, i) => hashes.findIndex((h2) => h2 === h) !== i)) {
+        console.error('Generated invalid mapping.');
+        return;
+    }
 
     const hashLength = (numHashes - 1).toString(base).length;
     hashes = hashes.map((h) => h.toString(base).padStart(hashLength, '0'));
